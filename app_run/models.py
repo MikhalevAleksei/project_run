11273f7b3bec0fa80f415f3cfb6e3b0d6e293a4b
Алексей Михалев <aleksey.mikhalev@osinit.com>
2025-10-01 18:25:56 +0300

    Second task ready

--- app_run/models.py
+++ app_run/models.py
@@ -1,19 +1,9 @@
 from django.db import models
-
-
-class User(models.Model):
-    athlete = models.CharField(max_length=100)
-
-    def __str__(self):
-        return self.athlete
+from django.contrib.auth.models import User
 
 
 class Run(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
-    athlete = models.ForeignKey(User, on_delete=models.CASCADE)
     comment = models.TextField()
-
-    def __str__(self):
-        return self.athlete
-
+    athlete = models.ForeignKey(User, on_delete=models.CASCADE)
 
